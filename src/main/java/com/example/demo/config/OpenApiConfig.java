58d3a6a5f2b357b774578b9b787068ed85fc61a6
bala-amypo <bala@amypo.in>
2025-12-23 04:36:32 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,18 +1,21 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                .title("Vendor SLA Performance Tracker API")
-                .version("1.0")
-                .description("API documentation for the Vendor SLA Performance Tracker project")); 
-    }
+                // You need to change the port as per your server
+                .servers(List.of(
+                        // new Server().url("https://9005.vs.amypo.ai")
+                        new Server().url("https://9103.408procr.amypo.ai/")
+                ));
+        }
 }
