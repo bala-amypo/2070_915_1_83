bf4b1a43c18906c747dde6a6ceceec918e71f7b1
bala-amypo <bala@amypo.in>
2025-12-29 08:25:12 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -10,13 +10,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleNotFound(RuntimeException ex) {
         if (ex.getMessage().contains("not found")) {
-            return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND); // [cite: 157]
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND); 
         }
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleValidation(IllegalArgumentException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST); // [cite: 160]
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST); 
     }
 }
